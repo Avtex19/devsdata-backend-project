c2e801a41de81410d7fb67d18768ed1b01bad804
Avtex19 <atukaberadze19@gmail.com>
2024-10-01 20:15:24 +0400

    refactor: change api url routes

--- merp_project/merp_app/api/urls.py
+++ merp_project/merp_app/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import getRoutes
 from . import views
-from .views import ViewEvents,CreateReservation
+from .views import ViewEvents,CreateReservation,GetReservation,CancelReservation
+
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('viewEvents/', ViewEvents.as_view()),
-    path('createReservation/', CreateReservation.as_view()),
+    path('events/', ViewEvents.as_view()),
+    path('reservation/create', CreateReservation.as_view()),
+    path('reservation/<uuid:reservation_code>/', GetReservation.as_view(), name='get_reservation'),
+    path('reservation/cancel/<uuid:reservation_code>/', CancelReservation.as_view())
 
 ]
--- merp_project/merp_app/api/views.py
+++ merp_project/merp_app/api/views.py
@@ -1,12 +1,9 @@
-import uuid
-from datetime import datetime
-
-from django.shortcuts import render
+from datetime import datetime, timedelta
+from ..random_sequence_generator import generate_unique_code
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from ..models import Event, Reservation
-from .serializers import EventSerializer, ReservationSerializer
+from .serializers import EventSerializer, ReservationSerializer, ReservationCancellationSerializer
 from rest_framework import generics, status
 
 
@@ -14,7 +11,9 @@ from rest_framework import generics, status
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
-        ''
+        'events'
+        'reservation/',
+        'reservation/create',
     ]
     return Response(routes)
 
@@ -28,6 +27,9 @@ class CreateReservation(generics.CreateAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
+    unique_code = generate_unique_code()
+    print(unique_code)
+
     def create(self, request, *args, **kwargs):
         event_id = request.data.get('event')
         print(request.data)
@@ -40,7 +42,7 @@ class CreateReservation(generics.CreateAPIView):
         except Event.DoesNotExist:
             return Response({"error": "Event not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        reservation_code = uuid.uuid4()
+        reservation_code = generate_unique_code()
         print(reservation_code)
 
         serializer = self.get_serializer(data=request.data)
@@ -52,3 +54,36 @@ class CreateReservation(generics.CreateAPIView):
             }, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetReservation(generics.RetrieveAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    lookup_field = 'reservation_code'
+
+
+class CancelReservation(generics.DestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationCancellationSerializer
+    lookup_field = 'reservation_code'
+
+    def destroy(self, request, *args, **kwargs):
+        reservation = self.get_object()
+        event = reservation.event
+
+        event_start_datetime = datetime.combine(event.start_date, datetime.min.time())
+        event_end_datetime = datetime.combine(event.end_date, datetime.min.time())
+
+        event_duration = event_end_datetime - event_start_datetime
+
+        if event_start_datetime <= datetime.now() + timedelta(days=2):
+            return Response({"error": "You cannot cancel this reservation within two days of the event."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if event_duration > timedelta(days=2):
+            return Response({"error": "You cannot cancel this reservation for events longer than two days."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        reservation.delete()
+
+        return Response({"message": "Reservation canceled successfully."}, status=status.HTTP_200_OK)
