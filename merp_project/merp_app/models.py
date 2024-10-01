4294b7e712bb561017ac1055914fad468fdf5757
Avtex19 <atukaberadze19@gmail.com>
2024-10-01 15:47:39 +0400

    add models and uploaded data in db

--- merp_project/merp_app/models.py
+++ merp_project/merp_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Reservation(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='reservations')
+    email = models.EmailField()
+    reservation_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    status = models.CharField(max_length=50, default='active')
+    created_at = models.DateTimeField(auto_now_add=True)
