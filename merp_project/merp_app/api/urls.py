87ced24a01eff1d601baa0d51ca6ab417772ee48
Avtex19 <atukaberadze19@gmail.com>
2024-10-01 18:03:08 +0400

    Implement reservation creation functionality for users to register for events

--- merp_project/merp_app/api/urls.py
+++ merp_project/merp_app/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import getRoutes
 from . import views
-from .views import ViewEvents
+from .views import ViewEvents,CreateReservation
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('viewEvents/', ViewEvents.as_view())
+    path('viewEvents/', ViewEvents.as_view()),
+    path('createReservation/', CreateReservation.as_view()),
 
 ]
--- merp_project/merp_app/api/views.py
+++ merp_project/merp_app/api/views.py
@@ -1,10 +1,13 @@
+import uuid
+from datetime import datetime
+
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from ..models import Event, Reservation
 from .serializers import EventSerializer, ReservationSerializer
-from rest_framework import generics
+from rest_framework import generics, status
 
 
 # Create your views here.
@@ -19,3 +22,33 @@ def getRoutes(request):
 class ViewEvents(generics.ListAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+class CreateReservation(generics.CreateAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+    def create(self, request, *args, **kwargs):
+        event_id = request.data.get('event')
+        print(request.data)
+        print(event_id)
+        if not event_id:
+            return Response({"error": "Event ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            event = Event.objects.get(id=event_id)
+        except Event.DoesNotExist:
+            return Response({"error": "Event not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        reservation_code = uuid.uuid4()
+        print(reservation_code)
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(event=event, reservation_code=reservation_code)
+            return Response({
+                "message": "Registration successful.",
+                "reservation_code": str(reservation_code)
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
