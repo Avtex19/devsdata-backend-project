f90ff98230adf87e5d2cdf1b5f9cf8bc05306303
Avtex19 <atukaberadze19@gmail.com>
2024-10-01 23:36:42 +0400

    refactor: change start_date and end_date from DateField to DateTime field and make it naive

--- merp_project/merp_app/models.py
+++ merp_project/merp_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from .random_sequence_generator import generate_unique_code
+from django.utils import timezone
+
 
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=250)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
 
     def __str__(self):
@@ -15,6 +17,6 @@ class Event(models.Model):
 class Reservation(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='reservations')
     email = models.EmailField()
-    reservation_code = models.CharField(default=generate_unique_code(),max_length=6, editable=False, unique=True)
+    reservation_code = models.CharField(default=generate_unique_code(), max_length=6, editable=False, unique=True)
     status = models.CharField(max_length=50, default='active')
     created_at = models.DateTimeField(auto_now_add=True)
