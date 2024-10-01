9b7eaa0cb93aef7410ac05e4107fd9455203989e
Avtex19 <atukaberadze19@gmail.com>
2024-10-01 23:38:29 +0400

    refactor: change reservation and reservation/cancel API endpoint's pks from uuid to str

--- merp_project/merp_app/api/urls.py
+++ merp_project/merp_app/api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.getRoutes),
     path('events/', ViewEvents.as_view()),
     path('reservation/create', CreateReservation.as_view()),
-    path('reservation/<uuid:reservation_code>/', GetReservation.as_view(), name='get_reservation'),
-    path('reservation/cancel/<uuid:reservation_code>/', CancelReservation.as_view())
+    path('reservation/<str:reservation_code>/', GetReservation.as_view(), name='get_reservation'),
+    path('reservation/cancel/<str:reservation_code>/', CancelReservation.as_view())
 
 ]
