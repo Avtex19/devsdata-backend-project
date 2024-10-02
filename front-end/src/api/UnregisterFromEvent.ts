687559b1a5ea11f8c0d7821992a777f863e9c3ae
Avtex19 <atukaberadze19@gmail.com>
2024-10-02 22:37:24 +0400

    feat: Add unregisterFromEvent function to handle event cancellation
    - Implemented unregisterFromEvent function to confirm cancellation and send a DELETE request to the server.
    - Prompts user for confirmation before proceeding with cancellation.
    - Handles errors and ensures proper API communication for unregistering from events.

--- front-end/src/api/UnregisterFromEvent.ts
+++ front-end/src/api/UnregisterFromEvent.ts
@@ -21,12 +21,6 @@ export const unregisterFromEvent = async (reservationCode: string): Promise<void
 
         return;
     } catch (error) {
-        if (error instanceof Error) {
-            // Rethrow the error with its message
-            throw new Error(error.message);
-        } else {
-            // In case the error is not an instance of Error, throw a generic error
-            throw new Error('An unexpected error occurred');
-        }
+        throw new Error(error as string);
     }
 };
