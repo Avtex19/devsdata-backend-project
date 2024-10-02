56ab144ec37615cb7c732730f81a0f34729ab751
Avtex19 <atukaberadze19@gmail.com>
2024-10-02 22:40:36 +0400

    feat: Add unregisterFromEvent function to handle event cancellation
    - Implemented unregisterFromEvent function to confirm cancellation and send a DELETE request to the server.
    - Prompts user for confirmation before proceeding with cancellation.
    - Handles errors and ensures proper API communication for unregistering from events.

--- front-end/src/api/UnregisterFromEvent.ts
+++ front-end/src/api/UnregisterFromEvent.ts
@@ -1,26 +1,20 @@
 const API_URL = 'http://localhost:8000/api/reservation/cancel/';
 
 export const unregisterFromEvent = async (reservationCode: string): Promise<void> => {
-    try {
-        const isConfirmed = window.confirm("Are you sure you want to unregister from this event?");
+    const isConfirmed = window.confirm("Are you sure you want to unregister from this event?");
 
-        if (!isConfirmed) {
-            return;
-        }
-
-        const response = await fetch(`${API_URL}${reservationCode}/`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error('Failed to unregister from the event');
-        }
-
+    if (!isConfirmed) {
         return;
-    } catch (error) {
-        throw new Error(error as string);
+    }
+
+    const response = await fetch(`${API_URL}${reservationCode}/`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to unregister from the event');
     }
 };
